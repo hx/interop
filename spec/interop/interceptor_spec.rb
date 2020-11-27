b6e48a8d0aa80a0f5b365b52ec35ad5cc0e43d15
Neil E. Pearson <neil.pearson@bigcommerce.com>
2020-11-27 22:14:44 +1100

    Linting

--- spec/interop/interceptor_spec.rb
+++ spec/interop/interceptor_spec.rb
@@ -76,7 +76,7 @@ module Hx::Interop
     end
 
     describe '.build' do
-      let(:conn) { Connection.new STDIN, STDOUT }
+      let(:conn) { Connection.new StringIO.new, StringIO.new }
 
       it 'can build a connection' do
         int = described_class.build conn do
