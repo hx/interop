2c5bbf554ea725ae90a0182566d438d5c6809593
Neil E. Pearson <neil.pearson@bigcommerce.com>
2021-08-11 09:47:31 +1000

    Prevent Go's bgRunner Start and Wait functions from racing

--- interop/bg_runner.go
+++ interop/bg_runner.go
@@ -1,11 +1,18 @@
 package interop
 
+import "sync"
+
 type bgRunner struct {
 	runner interface {
 		Run() error
 	}
-	blocker chan struct{}
-	err     error
+	initOnce sync.Once
+	blocker  chan struct{}
+	err      error
+}
+
+func (b *bgRunner) init() {
+	b.initOnce.Do(func() { b.blocker = make(chan struct{}) })
 }
 
 // Start calls the receiver's Run command in a new goroutine. Wait will block and return its error when it finishes.
@@ -13,15 +20,16 @@ func (b *bgRunner) Start() {
 	if b.runner == nil {
 		panic("nothing to start")
 	}
-	b.blocker = make(chan struct{})
+	b.init()
 	go func() {
 		b.err = b.runner.Run()
 		close(b.blocker)
 	}()
 }
 
-// Wait for the process to finish running in the background, returning its error. Panics unless Start has been called.
+// Wait for the process to finish running in the background, returning its error.
 func (b *bgRunner) Wait() error {
+	b.init()
 	<-b.blocker
 	return b.err
 }
